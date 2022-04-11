c37b4b9f235e14111235265b7e8283d2d8475d8e
Amir Hafez <amirhafez69@gmail.com>
2022-04-11 22:27:33 +0430

    minor changes to html + completing computerPlay and playerPlay functions

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-alert("Hello world!");
+let playerSelection;
+let computerSelection;
+
+function computerPlay() {
+    return Math.floor(Math.random() * 3);
+}
+
+console.log(computerPlay());
+
+function playerPlay() {
+    playerSelection = prompt("Enter rock, paper or scissors:").toLowerCase();
+    switch (playerSelection) {
+        case "paper":
+            return 0;
+            break;
+
+        case "rock":
+            return 1;
+            break;
+
+        case "scissors":
+            return 2;
+            break;
+
+        default:
+            alert("Invalid input!");
+            playerPlay();
+    }
+}
+
+let playerWins = 0;
+let computerWins = 0;
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        computerSelection = computerPlay();
+        playerSelection = playerPlay();
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+game();
